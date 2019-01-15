de04e9e7eeedfc571a054be623c36ff24a7a1ac0
babaleo <paulmuruthi@gmail.com>
2019-01-16 00:50:07 +0300

    changed container to container fluid to use up more space

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -167,3 +167,63 @@ function jsonToBarChart(requestURL,dataKey,xKey,xReverse,yKey,yDivisor,startStep
         new Chartist.Bar("#" + chartDivId, data, options);
     }
 }
+
+function jsonToComparisonBarChart(requestURL,dataKey,xKey,xReverse,yKey1,yKey2,yDivisor,startStep,endStep,chartDivId) {
+
+    if (!document.querySelector('.chartlist') && window.matchMedia("(min-width: 576px)").matches) {
+        var chartDiv = document.getElementById(chartDivId);
+        chartDiv.classList.remove('ct-minor-sixth');
+        chartDiv.classList.add('ct-double-octave');
+    }
+
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        var jsonObj = request.response;
+        var years = [];
+        var values1 = [];
+        var values2 = [];
+
+        if (xReverse == true) {
+            for (var step = startStep; step >= endStep; step--) {
+                years.push((jsonObj[dataKey][step][xKey]).slice(2,4));
+                if (yDivisor) {
+                    values1.push((jsonObj[dataKey][step][yKey1])/yDivisor);
+                    values2.push((jsonObj[dataKey][step][yKey2])/yDivisor);
+                } else {
+                    values1.push(jsonObj[dataKey][step][yKey1]);
+                    values2.push(jsonObj[dataKey][step][yKey2]);
+                }
+            }
+        } else {
+            for (var step = startStep; step <= endStep; step++) {
+                years.push((jsonObj[dataKey][step][xKey]).slice(2,4));
+                if (yDivisor) {
+                    values1.push((jsonObj[dataKey][step][yKey1])/yDivisor);
+                    values2.push((jsonObj[dataKey][step][yKey2])/yDivisor);
+                } else {
+                    values1.push(jsonObj[dataKey][step][yKey1]);
+                    values2.push(jsonObj[dataKey][step][yKey2]);
+                }
+            }
+        }
+
+        var data = {
+        // A labels array that can contain any sort of values
+        labels: years,
+        // Our series array that contains series objects or in this case series data arrays
+        series: [values1, values2]
+        };
+        var options = {
+            axisY: {
+                offset: 30,
+            }
+        };
+        // Create a new line chart object where as first parameter we pass in a selector
+        // that is resolving to our chart container element. The Second parameter
+        // is the actual data object.
+        new Chartist.Bar("#" + chartDivId, data, options);
+    }
+}
